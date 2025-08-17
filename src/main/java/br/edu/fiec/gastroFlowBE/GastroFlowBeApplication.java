0a7c9bcd7b10473bd5dba77e6eeb9b860cbe9fb0
Gustavo Soares <gustavo.soares220@hotmail.com>
2025-08-17 12:00:14 -0300

    Commit Inicial

---
+++ src/main/java/br/edu/fiec/gastroFlowBE/GastroFlowBeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fiec.gastroFlowBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroFlowBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroFlowBeApplication.class, args);
+	}
+
+}
